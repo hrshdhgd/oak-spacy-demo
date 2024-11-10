9c0185e61d925eb5e74f6271be9763d79791f1a6
Harshad <hrshdhgd@users.noreply.github.com>
2024-11-09 20:58:32 -0600

    first commit

---
+++ src/oak_spacy_demo/cli.py
@@ -0,0 +1,44 @@
+"""Command line interface for oak-spacy-demo."""
+import logging
+
+import click
+
+from oak_spacy_demo import __version__
+from oak_spacy_demo.main import demo
+
+__all__ = [
+    "main",
+]
+
+logger = logging.getLogger(__name__)
+
+
+@click.group()
+@click.option("-v", "--verbose", count=True)
+@click.option("-q", "--quiet")
+@click.version_option(__version__)
+def main(verbose: int, quiet: bool):
+    """
+    CLI for oak-spacy-demo.
+
+    :param verbose: Verbosity while running.
+    :param quiet: Boolean to be quiet or verbose.
+    """
+    if verbose >= 2:
+        logger.setLevel(level=logging.DEBUG)
+    elif verbose == 1:
+        logger.setLevel(level=logging.INFO)
+    else:
+        logger.setLevel(level=logging.WARNING)
+    if quiet:
+        logger.setLevel(level=logging.ERROR)
+
+
+@main.command()
+def run():
+    """Run the oak-spacy-demo's demo command."""
+    demo()
+
+
+if __name__ == "__main__":
+    main()
